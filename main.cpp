a7153ea7822aa9c388380af558b8593aea797e92
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-24 16:17:46 +0200

    Completed Task 1

--- main.cpp
+++ main.cpp
@@ -1,12 +1,87 @@
 #include <iostream>
 #include <conio.h>
+#include <random>
 
 using std::cout;
 using std::endl;
 using std::cin;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+
+template<typename TValueL>
+void delete_value(TValueL*& value, char indicator)
+{
+    delete value;
+    value = nullptr;
+}
+template<typename TValueL>
+void delete_value(TValueL*& arr, bool indicator)
+{
+    delete[] arr;
+    arr = nullptr;
+}
+
+template<typename TValue>
+void init(TValue* arr, int* size)
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dist(0, 99);
+
+
+    for (int i = 0; i < *size; i++)
+    {
+        arr[i] = dist(gen);
+    }
+}
+
+template<typename TValueL, typename TValueR>
+void print(TValueL* value, TValueR* message)
+{
+    cout << message << *value << endl;
+}
+template<typename TValueL, typename TValueR>
+void print(TValueL* arr, int* size, TValueR* message)
+{
+    cout << message;
+
+    for (int i = 0; i < *size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+template<typename TValue>
+void sorting(TValue* arr, int* size)
+{
+    for (int i = 0; i < *size - 1; i++)
+    {
+        for (int j = 0; j < *size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
 int main()
 {
+    int* size = new int{ 10 };
+    int* arr = new int[*size];
+    init<int>(arr, size);
+    print<int, const char>(arr, size, "Array: ");
+
+    sorting<int>(arr, size);
+    print<int, const char>(arr, size, "Result: ");
+
+    delete_value<int>(arr, true);
+    delete_value<int>(size, ' ');
 
 	char ch = _getch();
 	return 0;
