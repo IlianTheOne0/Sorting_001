76cb3931c70f4e59fe09f50a59a524ba35cb1259
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-26 17:28:15 +0200

    Completed Task 1

--- main.cpp
+++ main.cpp
@@ -32,6 +32,13 @@ void incorrect()
     SetConsoleTextAttribute(hConsole, 7);
 }
 
+void sorted()
+{
+    SetConsoleTextAttribute(hConsole, 2);
+    cout << "Sorted!" << endl;
+    SetConsoleTextAttribute(hConsole, 7);
+}
+
 
 template<typename TValue>
 void init(TValue* arr, int* size, int min, int max)
@@ -59,11 +66,55 @@ void print(TValueL* arr, int* size, TValueR* message)
 
     for (int i = 0; i < *size; i++)
     {
-        cout << "\t" << i << ". " << arr[i] << endl;
+        cout << "\t" << i + 1 << ". " << arr[i] << endl;
     }
     cout << endl;
 }
 
+template<typename TValue>
+void sorting(TValue* arr, int* size)
+{
+    for (int i = 0; i < *size - 2; i += 2)
+    {
+        for (int j = 0; j < *size - i - 2; j += 2)
+        {
+            if (arr[j] > arr[j + 2])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 2];
+                arr[j + 2] = temp;
+
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j + 3];
+                arr[j + 3] = temp;
+            }
+        }
+    }
+}
+
+template<typename TValue>
+void compare(TValue* arr0, TValue* arr1, int add0, int add1, int* size)
+{
+    int* new_size = new int{ *size * 2 };
+    int* new_arr = new int[*new_size] {};
+
+    for (int j = 0; j < *size; j++)
+    {
+        new_arr[j * 2 + add0] = arr0[j];
+        new_arr[j * 2 + add1] = arr1[j];
+    }
+
+    sorting(new_arr, new_size);
+
+    for (int j = 0; j < *size; j++)
+    {
+        arr0[j] = new_arr[j * 2 + add0];
+        arr1[j] = new_arr[j * 2 + add1];
+    }
+
+    delete_value<int>(new_arr, true);
+    delete_value<int>(new_size, ' ');
+}
 
 int main()
 {
@@ -88,17 +139,60 @@ int main()
 
         if (choice > 4 || choice < 1)
         {
+
+            system("cls");
             incorrect();
             continue;
         }
+        else
+        {
+            switch (choice)
+            {
+                case 1:
+                {
+                    system("cls");
+
+                    compare<int>(arr0, arr1, 0, 1, size);
+                    sorted();
+
+                    cout << endl << endl << "Press any key to continue..." << endl;
+                    char ch = _getch();
+                    system("cls");
+                } break;
+                
+
+                case 2:
+                {
+                    system("cls");
+                    
+                    compare<int>(arr0, arr1, 1, 0, size);
+                    sorted();
+
+                    cout << endl << endl << "Press any key to continue..." << endl;
+                    char ch = _getch();
+                    system("cls");
+                } break;
+
+                case 3:
+                {
+                    system("cls");
+
+                    print<int, const char>(arr0, size, "Mobile phone numbers: ");
+                    print<int, const char>(arr1, size, "Home phone numbers: ");
+
+                    cout << endl << endl << "Press any key to continue..." << endl;
+                    char ch = _getch();
+                    system("cls");
+                } break;
+            }
+        }
     } while (choice != 4);
 
-    /*print<int, const char>(arr0, size, "Mobile phone numbers: ");
-    print<int, const char>(arr1, size, "Home phone numbers: ");*/
+
     delete_value<int>(arr0, true);
     delete_value<int>(arr1, true);
     delete_value<int>(size, ' ');
 
-    char ch = _getch();
+    //char ch = _getch();
     return 0;
 }
